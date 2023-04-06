02cae58411e03707ceef3588d05ddf00a0a05a9e
dedenamikom <128024041+dedenamikom@users.noreply.github.com>
2023-04-06 14:53:56 +0700

    Add files via upload

---
+++ PlaneTiket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberKelas
+{
+    class PlaneTiket
+    {
+        public string origin { get; set; }
+        public int cost { get; set; }
+        public string currency { get; set; }
+        public string destination { get; set; }
+
+        public void printSummary()
+        {
+            Console.WriteLine("Origin\t\t= {0}", origin);
+          
+            Console.WriteLine("destination\t= {0}", destination);
+            Console.WriteLine("cost\t\t= {0}({1})", cost,currency);
+            if (currency == "IDR")
+            {
+                int dolar = cost / 15000;
+                Console.WriteLine("Convert to Dolar : {0}", dolar);
+
+            }
+            else if(currency == "USD")
+            {
+                int idr = cost * 15000;
+                Console.WriteLine("Convert to  Rupiah : {0}", idr);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberKelas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlaneTiket tiket1 = new PlaneTiket();
+
+            PlaneTiket tiket2 = new PlaneTiket();
+
+
+            tiket1.origin = "JOGJA";
+            tiket1.currency = "USD";
+            tiket1.destination = "PKU";
+            tiket1.cost = 100;
+
+            tiket2.origin = "JKT";
+            tiket2.currency = "IDR";
+            tiket2.destination = "PKU";
+            tiket2.cost = 750000;
+
+            tiket1.printSummary();
+            tiket2.printSummary();
+            Console.ReadKey();
+        }
+    }
+}
